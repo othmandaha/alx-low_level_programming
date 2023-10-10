6bdf38140cde05eecbb4790e1700792ec9b4f0d8
othmandaha <dahaothman02@gmail.com>
2023-10-10 21:03:42 +0100

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * print_alphabet - For printing alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+
+	void print_alphabet(void)
+	{
+		int i = 0;
+
+		char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+
+		for (i = 0; i < 26; i++)
+		{
+			_putchar(alphabet[i]);
+		}
+		_putchar(10);
+	}
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
