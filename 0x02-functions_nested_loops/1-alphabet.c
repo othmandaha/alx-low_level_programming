0d632086a74c0fdaac2ecd77738077f3bc03cb4c
othmandaha <dahaothman02@gmail.com>
2023-10-10 21:15:18 +0100

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,15 +8,15 @@
  * Return: Always 0 (Success)
  */
 
-	void print_alphabet(void)
+void print_alphabet(void)
+{
+	int i = 0;
+
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+
+	for (i = 0; i < 26; i++)
 	{
-		int i = 0;
-
-		char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
-
-		for (i = 0; i < 26; i++)
-		{
-			_putchar(alphabet[i]);
-		}
-		_putchar(10);
+		_putchar(alphabet[i]);
 	}
+	_putchar(10);
+}
