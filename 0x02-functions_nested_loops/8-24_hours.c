a65c340bfcbc5db22573c3f67633c1c997970b90
othmandaha <dahaothman02@gmail.com>
2023-10-11 00:54:11 +0100

    printing all minutes of the whole day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,8 +10,12 @@ void jack_bauer(void)
 
 	for (a = 0; a < 3; a++)
 	{
-		for (b = 0; b < 4; b++)
+		for (b = 0; b <= 9; b++)
 		{
+			if (b == 9)
+			{
+				b = 0
+			}
 			for (c = 0; c < 6; c++)
 			{
 				for (d = 0; d < 10; d++)
