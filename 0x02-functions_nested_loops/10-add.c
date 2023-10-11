30192459b78ce4f3bef745a517207d87d4f365b4
othmandaha <dahaothman02@gmail.com>
2023-10-11 03:07:12 +0100

    addition functin

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main."
+
+/**
+ * add - adding two ingtegers
+ *
+ * @a: int to be added to b
+ * @b: int to be added to a
+ * Return: value of the addition of a and b
+ */
+
+int add(int a, int b)
+{
+	int v;
+
+	v = a + b;
+
+	return (v);
+}
