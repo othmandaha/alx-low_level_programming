04d4711a9c8c4717a74c8900dae2c59c7a3b7251
othmandaha <dahaothman02@gmail.com>
2023-10-11 03:00:21 +0100

    the 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,46 +1,37 @@
 #include "main.h"
 
 /**
- * times_table - printing the 9 times table
+ * times_table - prints the 9 times table, starting with 0
  */
-
 void times_table(void)
 {
-	int i, j, r;
+	int i, j, k;
 
-	i = 0;
-	j = 0;
-	while (i <= 9)
+	for (i = 0; i < 10; i++)
 	{
-		while (j <= 9)
+		for (j = 0; j < 10; j++)
 		{
-			r = i * j;
-
-			if (r < 10)
+			k = j * i;
+			if (j == 0)
 			{
-				_putchar(r + '0');
-				if (j != 9)
-				{
-					_putchar(',');
-				}
-				_putchar(' ');
-				_putchar(' ');
-			}
-			else
-			{
-				_putchar((r / 10) + '0');
-				_putchar((r % 10) + '0');
-				if (j != 9)
-				{
-					_putchar(',');
-				}
+				_putchar(k + '0');
 			}
 
-		j++;
+			if (k < 10 && j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
+			} else if (k >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+			}
 		}
-		if (j == 10)
-			j = 0;
-	_putchar(10);
-	i++;
+		_putchar('\n');
 	}
 }
+
---
+++ 0x02-functions_nested_loops/mine_9-times_table.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * times_table - printing the 9 times table
+ */
+
+void times_table(void)
+{
+	int i, j, r;
+
+	i = 0;
+	j = 0;
+	while (i <= 9)
+	{
+		while (j <= 9)
+		{
+			r = i * j;
+
+			if (r < 10)
+			{
+				_putchar(r + '0');
+				if (j != 9)
+				{
+					_putchar(',');
+				}
+				_putchar(' ');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar((r / 10) + '0');
+				_putchar((r % 10) + '0');
+				if (j != 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+
+		j++;
+		}
+		if (j == 10)
+			j = 0;
+	_putchar(10);
+	i++;
+	}
+}
