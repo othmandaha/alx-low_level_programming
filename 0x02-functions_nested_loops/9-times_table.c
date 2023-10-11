e9feebfcb14c21cc0f76be8151d1be5921a4bb5d
othmandaha <dahaothman02@gmail.com>
2023-10-11 02:38:49 +0100

    the 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -20,9 +20,7 @@ void times_table(void)
 			{
 			_putchar(r + '0');
 			if (j != 9)
-			{
 				_putchar(',');
-			}
 			_putchar(' ');
 			_putchar(' ');
 			}
@@ -31,9 +29,7 @@ void times_table(void)
 				_putchar((r / 10) + '0');
 				_putchar((r % 10) + '0');
 				if (j != 9)
-				{
 					_putchar(',');
-				}
 				_putchar(' ');
 
 		j++;
