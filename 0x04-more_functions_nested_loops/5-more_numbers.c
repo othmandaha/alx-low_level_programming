38c3cf332d597d126e1d0bcfdaef26c18e5bb61a
othmandaha <dahaothman02@gmail.com>
2023-10-12 16:09:01 +0100

    more numbers function

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - print numbers to 14 ten times
+ */
+
+void more_numbers(void)
+{
+	char num[] = "01234567891011121314";
+
+	int i = 0;
+
+	int j = 0;
+
+	while (j < 10)
+	{
+		while (i <= 19)
+		{
+			_putchar(num[i]);
+		i++
+		}
+		_putchar(10);
+	j++
+	}
+}
