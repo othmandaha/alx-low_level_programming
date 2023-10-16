5d3b30aecd6f56bca43f110e2b946b262b6e7199
othmandaha <dahaothman02@gmail.com>
2023-10-17 00:19:57 +0100

    copying string function

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcpy - a function for copying a string
+ * @dest: the array to hold the copy
+ * @src: the string to be copied
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int length = len(*src);
+	int i;
+
+	for(i = 0; i <= length; i++)
+	{
+		dest[i] = *(src + i);
+		i++;
+	}
+	return (*src);
+}
