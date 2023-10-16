a81e40baea13eda5238c928ccdf69d46026a1d9e
othmandaha <dahaothman02@gmail.com>
2023-10-16 16:07:25 +0100

    turning any int to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,11 +2,14 @@
 
 /**
  * rest_to_98- a function to makes *n 98
- * @n - address of an int
- */
+ * @n: address of an int
+ *
+ * Retrun: returns 0
 
 int rest_to_98(int *n)
 {
 	int b = 98;
 	*n = b;
+
+	return(0);
 }
