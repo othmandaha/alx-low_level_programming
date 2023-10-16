2186a83d0c26c831aa4a457491868e72e213df1f
othmandaha <dahaothman02@gmail.com>
2023-10-16 20:44:28 +0100

    revers string function

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * rev_string - revers a string 
+ * @s: the string to be reversed
+ */
+
+void rev_string(char *s)
+{
+	int length = sizeof(*s);
+	length--;
+
+	while (length >= 0)
+	{
+		_putchar(*s[length]);
+		length--;
+	}
+	_putchar(10);
+}
