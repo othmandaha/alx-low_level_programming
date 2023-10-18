53f55b326d40a400f94a554a43fab11bca9a9109
othmandaha <dahaothman02@gmail.com>
2023-10-18 19:56:24 +0100

    concatenating with specefications

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncat - concatenating two string but use n bytes from src
+ * @dest: destination of the concatenation
+ * @src: the source from which to derive the string to concatenate
+ * @n: the numbers of bytes to take from src
+ * Return: the full concatenated string (dest)
+ */
+
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int d = strlen(dest);
+	int s = strlen(src);
+	int i;
+
+	while (i < n && i < s)
+	{
+		dest[d] = src[s];
+		i++;
+		d++;
+	}
+
+	return (dest);
---
+++ 0x06-pointers_arrays_strings/testing.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+char *_strncat(char *dest, char *src, int n);
+
+int main(void)
+{
+    char s1[98] = "Hello ";
+    char s2[] = "World!\n";
+    char *ptr;
+
+    printf("%s\n", s1);
+    printf("%s", s2);
+    ptr = _strncat(s1, s2, 1);
+    printf("%s\n", s1);
+    printf("%s", s2);
+    printf("%s\n", ptr);
+    ptr = _strncat(s1, s2, 1024);
+    printf("%s", s1);
+    printf("%s", s2);
+    printf("%s", ptr);
+return (0);
+}
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int d = strlen(dest);
+	int s = strlen(src);
+	int i = 0;
+
+	while (i < n && i < s)
+	{
+		dest[d] = src[i];
+		i++;
+		d++;
+	}
+
+	return (dest);
+}
