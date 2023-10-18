536a1fbbe7cbbc221b63ab75d24d2d143758e296
othmandaha <dahaothman02@gmail.com>
2023-10-19 00:38:41 +0100

    toupper function

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -8,18 +8,18 @@
 
 char *string_toupper(char *s)
 {
-        int i = 0;
-        int tmp;
+	int i = 0;
+	int tmp;
 
-        while (s[i] != '\0')
-        {
-                if (s[i] <= 'z' && s[i] >= 'a')
-                {
-                        s[i] -= 32;
-                }
-                i++;
+	while (s[i] != '\0')
+	{
+		if (s[i] <= 'z' && s[i] >= 'a')
+		{
+			s[i] -= 32;
+		}
+		i++;
 
-        }
+	}
 	return (s);
 
 }
