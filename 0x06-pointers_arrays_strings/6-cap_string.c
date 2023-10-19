028ca062e7c28e1ddc189708a4982ccc9e247a4f
othmandaha <dahaothman02@gmail.com>
2023-10-19 13:31:59 +0100

    capitalizing char function

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,15 +1,16 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * cap_string - capitalize char in string afte specefic inidcators
+ * such as ',' ';' and so on.
+ * @s: the string to work with
+ * Return: it returns the result.
  */
 
 char *cap_string(char *s)
 {
 	int i = 0;
-	
+
 	while (s[i] != '\0')
 	{
 		if (s[i] == 32 || s[i] == 9 || s[i] == 10 || s[i] == 44 ||
@@ -28,15 +29,5 @@ char *cap_string(char *s)
 			i++;
 		}
 	}
-	return (s);	
-}
-int main(void)
-{
-    char str[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
-    char *ptr;
-
-    ptr = cap_string(str);
-    printf("%s", ptr);
-    printf("%s", str);
-    return (0);
+	return (s);
 }
