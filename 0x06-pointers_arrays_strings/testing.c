2f27093c0c6e4636845270a2f80d077f1eb63d54
othmandaha <dahaothman02@gmail.com>
2023-10-19 13:49:16 +0100

    capitalizing char function

--- 0x06-pointers_arrays_strings/testing.c
+++ 0x06-pointers_arrays_strings/testing.c
@@ -9,7 +9,7 @@
 char *cap_string(char *s)
 {
 	int i = 0;
-	
+
 	while (s[i] != '\0')
 	{
 		if (s[i] == 32 || s[i] == 9 || s[i] == 10 || s[i] == 44 ||
@@ -17,18 +17,14 @@ char *cap_string(char *s)
 			s[i] == 34 || s[i] == 40 || s[i] == 41 ||
 			s[i] == 123 ||  s[i] == 125 || s[i] == 59)
 		{
-			i++;
-			if (s[i] <= 'z' && s[i] >= 'a')
+			if (s[i + 1] <= 'z' && s[i + 1] >= 'a')
 			{
-				s[i] = s[i] - 32;
+				s[i + 1] = s[i + 1] - 32;
 			}
 		}
-		else
-		{
-			i++;
-		}
+		i++;
 	}
-	return (s);	
+	return (s);
 }
 int main(void)
 {
