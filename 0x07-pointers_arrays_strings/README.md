8e6597318df3957d0329f3cc72c50f983e05246a
othmandaha <dahaothman02@gmail.com>
2023-10-23 15:15:06 +0100

    memset function

---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+this directory contains all the tasks of the projects 0x07-pointers_arrays_strings
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_h
+#define MAIN_h
+
+char *_memset(char *s, char b, unsigned int n);
+
+
+#endif 
