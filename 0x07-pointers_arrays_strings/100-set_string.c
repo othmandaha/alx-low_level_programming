6e47f421f7fdce70399ea202e3da7d2aa5094a57
othmandaha <dahaothman02@gmail.com>
2023-10-24 15:15:42 +0100

    set string funcion

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - it sets the value of a ponter to a char
+ * @s: the ponter to be set
+ * @to: the value to be set with
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
--- 0x07-pointers_arrays_strings/testing.c
+++ 0x07-pointers_arrays_strings/testing.c
@@ -1,42 +1,22 @@
 #include <stdio.h>
+
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
 
-void print_diagsums(int *a, int size)
+void set_string(char **s, char *to)
 {
-	int i = 0;
-	int j = 0;
-	int first_sum = 0;
-	int second_sum = 0;
-
-	while (i < size)
-	{
-		first_sum += a[i * size + i];
-
-		second_sum += a[i - size - 1];
-
-		i++;
-	}
-	printf("%d, %d", first_sum, second_sum);
+	*s = to;
 }
 int main(void)
 {
-    int c3[3][3] = {
-        {0, 1, 5},
-        {10, 11, 12},
-        {1000, 101, 102},
-    };
-    int c5[5][5] = {
-        {0, 1, 5, 12124, 1234},
-        {10, 11, 12, 123521, 12512},
-        {1000, 101, 102, 12545, 214543435},
-        {100, 1012451, 11102, 12545, 214543435},
-        {10, 12401, 10452, 11542545, 1214543435},
-    };
-    print_diagsums((int *)c3, 3);
-    print_diagsums((int *)c5, 5);
+    char *s0 = "Bob Dylan";
+    char *s1 = "Robert Allen";
+
+    printf("%s, %s\n", s0, s1);
+    set_string(&s1, s0);
+    printf("%s, %s\n", s0, s1);
     return (0);
 }
