20366cd29d2e7a7d388807ceb0d036e895c40958
othmandaha <dahaothman02@gmail.com>
2023-10-25 18:12:10 +0100

    printing in reverse function

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -5,11 +5,11 @@
  * @s: the string to be printed
  */
 
-void _puts_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 	if (*(s) != '\0')
 	{
-	_puts_recursion(s + 1);
+	_print_rev_recursion(s + 1);
 	_putchar(*s);
 	}
 
