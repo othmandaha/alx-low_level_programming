50bd41e6eed08434e62084a530ac0469a970f6da
othmandaha <dahaothman02@gmail.com>
2023-10-25 22:25:50 +0100

    pow recursion function

--- 0x08-recursion/testing.c
+++ 0x08-recursion/testing.c
@@ -6,33 +6,36 @@
  * Return: Always 0.
  */
 
-int factorial(int n)
+int _pow_recursion(int x, int y)
 {
-	if (n == 0)
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
 	{
 		return (1);
-	}
-	else if (n > 0)
-	{ 
-		return(n * factorial(n - 1)); 
-		
-	}
-	if (n < 0)
+	}	
+	else
 	{
-		return(-1);
+		return (x * _pow_recursion(x, y - 1));
 	}
 }
 int main(void)
 {
     int r;
 
-    r = factorial(1);
+    r = _pow_recursion(1, 10);
     printf("%d\n", r);
-    r = factorial(5);
+    r = _pow_recursion(1024, 0);
     printf("%d\n", r);
-    r = factorial(10);
+    r = _pow_recursion(2, 16);
     printf("%d\n", r);
-    r = factorial(-1024);
+    r = _pow_recursion(5, 2);
+    printf("%d\n", r);
+    r = _pow_recursion(5, -2);
+    printf("%d\n", r);
+    r = _pow_recursion(-5, 3);
     printf("%d\n", r);
     return (0);
 }
