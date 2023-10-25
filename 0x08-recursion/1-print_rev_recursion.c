8baeae0ba7481c528d06c214e3567903651d2e1c
othmandaha <dahaothman02@gmail.com>
2023-10-25 18:10:06 +0100

    printing in reverse function

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - print a string in revers
+ * @s: the string to be printed
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*(s) != '\0')
+	{
+	_puts_recursion(s + 1);
+	putchar(*s);
+	}
+
+
+}
--- 0x08-recursion/testing.c
+++ 0x08-recursion/testing.c
@@ -1,24 +1,26 @@
 #include <stdio.h>
+#include <string.h>
 
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
+
+
 void _puts_recursion(char *s)
 {
-	if (*(s) == '\0')
+	if (*(s) != '\0')
 	{
-		putchar(10);
-		return;
-	}
-	putchar(*s);
 	_puts_recursion(s + 1);
+	putchar(*s);
+	}
+
 
 }
 
 int main(void)
 {
-    _puts_recursion("fuck you all I hate you");
+    _puts_recursion("\nfuck you all I hate you");
     return (0);
 }
