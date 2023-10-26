f328265ca3b0a5034555d598c8b4d8cb7850e287
othmandaha <dahaothman02@gmail.com>
2023-10-26 18:27:14 +0100

    wildcmp function

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -11,11 +11,11 @@
 
 int wildcmp(char *s1, char *s2)
 {
-	if (strlen(s1) != strlen(s2))
+	if (*(s2) == '*')
 	{
-		return (0);
+		return (1);
 	}
-	else if (*(s1) == '\0' && (*(s2) == '\0' || *(s2) == '*'))
+	else if (*(s1) == '\0' && *(s2) == '\0')
 	{
 		return (1);
 	}
