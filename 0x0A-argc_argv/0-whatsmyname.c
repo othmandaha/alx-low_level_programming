711345cb63aecd84f5a33f90d0825f29d4fd5791
othmandaha <dahaothman02@gmail.com>
2023-10-27 17:07:05 +0100

    whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - the starting point of the program
+ * @argc: argument count
+ * @argv: arguments
+ *
+ * Return: always 0 means success.
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+this is the directory for all the tasks related to "argc & argv" project
