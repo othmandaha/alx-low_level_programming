193ba1ff1e197244c5dfce1e4b98026d69535efb
othmandaha <dahaothman02@gmail.com>
2023-10-31 20:42:10 +0100

    strdup function

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include <string.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strdup - allocate momory to store a copy of str
+ * @str: the string to be copied
+ *
+ * Return: a pointer to the copied string
+ */
+
+char *_strdup(char *str)
+{
+	unsigned int size;
+	char *cpy;
+	int i;
+
+	size = strlen(str);
+	if (size == 0 || str[0] == '\0')
+		return (0);
+
+	cpy = malloc(size * sizeof(char));
+	while (i < size)
+	{
+		cpy[i] = str[i];
+		i++;
+	}
+	return (cpy);
+}
