df5dc1d404f54f8e01c3ef1d42401eb2d48db2a8
othmandaha <dahaothman02@gmail.com>
2023-11-02 16:38:12 +0100

    malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -13,10 +13,9 @@ void *malloc_checked(unsigned int b)
 {
 	int *c;
 
-	c = malloc(b);
-
-	if (c >= INT_MAX)
+	if (b >= INT_MAX)
 		return ((void *)98);
+	c = malloc(b);
 	if (c == NULL)
 		return ((void *)98);
 	else
