fd86465a7c4513d2aae88de8e96064e85c1b50c3
othmandaha <dahaothman02@gmail.com>
2023-11-02 18:33:55 +0100

    string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -22,7 +22,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
-		s2 = "";	
+		s2 = "";
 	s1_size = strlen(s1);
 	s2_size = strlen(s2);
 	if (s2_size < n)
