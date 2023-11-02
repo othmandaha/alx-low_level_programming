ef609f2bca66f19b75bd3ebfa9c14a4fc309a0bc
othmandaha <dahaothman02@gmail.com>
2023-11-02 16:18:52 +0100

    malloc checked

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: size of memory
+ *
+ * Return: adress if successful, 98 otherwise
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *c;
+
+	c = malloc(b);
+	if (c == NULL)
+		return (98);
+	else
+		return (c);
+}
+
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+this repo is a contuniation of 0x0B rep which is all about malloc and free function and the tasks related to those two functions
