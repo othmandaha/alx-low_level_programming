af9b6724a0287aa5ef8556592b18396bc5f983b3
othmandaha <dahaothman02@gmail.com>
2023-11-08 22:20:02 +0100

    index finder

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,17 +14,20 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i;
 
 	i = 0;
-	if (size <= 0)
-		return (-1);
-
-	while (i < size)
+	if (size > 0)
 	{
-		if (cmp(array[i]) != 0)
+		if (array != NULL && cmp != NULL)
 		{
-			return (i);
+			while (i < size)
+			{
+				if (cmp(array[i]) != 0)
+				{
+					return (i);
+				}
+				else
+					i++;
+			}
 		}
-		else
-			i++;
 	}
 	return (-1);
 }
