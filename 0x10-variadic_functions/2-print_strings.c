d1d792ac3c7a51a1149a1dfe3b30ee467cab3526
othmandaha <dahaothman02@gmail.com>
2023-11-09 19:06:51 +0100

    hprint strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -20,7 +20,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (separator != NULL && i != n - 1)
 		{
 			if (str)
-				printf("%s,%s", str, separator);
+				printf("%s%s", str, separator);
 			else
 				printf("nil%s", separator);
 		}
