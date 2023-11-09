e0181acdb3c11c241b0fc039a87432926ad0afdb
othmandaha <dahaothman02@gmail.com>
2023-11-09 16:30:11 +0100

    sum

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * sum_them_all - calculate the same of all its arguments
+ * @n: number of arguments
+ *
+ * Return: the sum of the arguments
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list pa;
+	int sum;
+
+	if (n == 0)
+		return (NULL);
+	sum = 0;
+	va_start(pa, n);
+	for (i = 0; i <= n; i++)
+	{
+		sum += va_arg(pa, int);
+	}
+	va_end(pa);
+	return (sum);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+this directory contains all the source codes files of the tasks related to the project of variadic functions
