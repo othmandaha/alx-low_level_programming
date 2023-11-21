d40240f1491a88ceeb2f4f4c3a2eb4737a25b441
othmandaha <dahaothman02@gmail.com>
2023-11-21 17:17:11 +0100

    delete node

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -26,7 +26,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	for (i = 0; i < index - 1; i++)
 	{
 		if (previous->next == NULL)
-			return(-1)
+			return (-1);
 		previous = previous->next;
 	}
 	current = previous->next;
