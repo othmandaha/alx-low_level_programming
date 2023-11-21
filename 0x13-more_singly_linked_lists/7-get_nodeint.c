bfe72aff27dfebdda96563e1b5aad14adbddde4e
othmandaha <dahaothman02@gmail.com>
2023-11-21 13:01:15 +0100

    updated get node

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -13,12 +13,12 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	unsigned int i;
 
 	if (head == NULL)
-		return (0);
+		return (NULL);
 	for (i = 0; i < index; i++)
 	{
 		head = head->next;
 		if (head == NULL)
-			return (0);
+			return (NULL);
 	}
 	return (head);
 }
