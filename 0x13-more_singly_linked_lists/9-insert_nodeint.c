313a1e18a6ab892d897472a77875bf480a3cadbb
othmandaha <dahaothman02@gmail.com>
2023-11-21 15:49:02 +0100

    insert nodeint

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -39,7 +39,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		new->next = *head;
 		*head = new;
 	}
-	tmp->next = new;
 	new->next = tmp->next;
+	tmp->next = new;
 	return (new);
 }
