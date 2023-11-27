efdfcbad66fc42bf3e7975607950968e3d41e68e
othmandaha <dahaothman02@gmail.com>
2023-11-27 20:53:04 +0100

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,17 +18,17 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content)
 	{
-		while (text_content(len) != '\0')
+		while (text_content[len] != '\0')
 		{
 			len++;
 		}
 	}
-	o = open(filename, O_CREATE | O_RDWR | O_TRUNC | 0600);
-	w = write(filename, text_content, len);
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC | 0600);
+	w = write(o, text_content, len);
 
 	if (o == -1 || w == -1)
 		return (-1);
 
-	close(0);
+	close(o);
 	return (1);
 }
