2bf9baf0b82031cb4de5fac34f5abb8e1e6e55a5
othmandaha <dahaothman02@gmail.com>
2023-11-27 20:49:27 +0100

    create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_file - creates a new file
+ * @filename: the name of the file to be created.
+ * @text_content: the content to be written in the file.
+ *
+ * Return: return 1 if succeful, -1 otherwise.
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int o, w, len = 0;
+
+	if (!filename)
+		return (-1);
+
+	if (text_content)
+	{
+		while (text_content(len) != '\0')
+		{
+			len++;
+		}
+	}
+	o = open(filename, O_CREATE | O_RDWR | O_TRUNC | 0600);
+	w = write(filename, text_content, len);
+
+	if (o == -1 || w == -1)
+		return (-1);
+
+	close(0);
+	return (1);
+}
