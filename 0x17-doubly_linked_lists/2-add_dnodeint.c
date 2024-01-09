d32f75bd4a107b08e1849549bfc97d67d74f9a2b
othmandaha <dahaothman02@gmail.com>
2024-01-09 13:41:59 +0100

    removed main

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -29,38 +29,3 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	return (new);
 }
-size_t print_dlistint(const dlistint_t *h)
-{
-        size_t s = 0;
-
-        if (!h)
-                return (s);
-
-        while (h->prev)
-                h = h->prev;
-
-        while (h)
-        {
-                printf("%d\n", h->n);
-                s++;
-                h = h->next;
-        }
-        return (s);
-}
-
-int main(void)
-{
-    dlistint_t *head;
-
-    head = NULL;
-    add_dnodeint(&head, 0);
-    add_dnodeint(&head, 1);
-    add_dnodeint(&head, 2);
-    add_dnodeint(&head, 3);
-    add_dnodeint(&head, 4);
-    add_dnodeint(&head, 98);
-    add_dnodeint(&head, 402);
-    add_dnodeint(&head, 1024);
-    print_dlistint(head);
-    return (EXIT_SUCCESS);
-}
